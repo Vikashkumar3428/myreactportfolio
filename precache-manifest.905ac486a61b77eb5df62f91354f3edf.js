c0e8d22558822fc689c67c316a62f540d17a6d81
Vikashkumar3428 <vkumar3428@gmail.com>
2020-10-07 22:59:57 +0530

    Updates

---
+++ precache-manifest.905ac486a61b77eb5df62f91354f3edf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86dbf02e7cd7a87868f4247b80dce96a",
+    "url": "/myreactportfolio/index.html"
+  },
+  {
+    "revision": "45ac9fb7a5a1abea50ab",
+    "url": "/myreactportfolio/static/css/2.4dc5d5af.chunk.css"
+  },
+  {
+    "revision": "f1014e3e50ea5775dc9a",
+    "url": "/myreactportfolio/static/css/main.22f3fda0.chunk.css"
+  },
+  {
+    "revision": "45ac9fb7a5a1abea50ab",
+    "url": "/myreactportfolio/static/js/2.52314419.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myreactportfolio/static/js/2.52314419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1014e3e50ea5775dc9a",
+    "url": "/myreactportfolio/static/js/main.d1eda491.chunk.js"
+  },
+  {
+    "revision": "db88d6dae3d929ba03c3",
+    "url": "/myreactportfolio/static/js/runtime-main.b7dc9adc.js"
+  },
+  {
+    "revision": "9e83403d78938cd251681f412cf9b3a3",
+    "url": "/myreactportfolio/static/media/2.9e83403d.jpg"
+  },
+  {
+    "revision": "9ca7591edec639d1f56a538fdd675e50",
+    "url": "/myreactportfolio/static/media/22589.9ca7591e.jpg"
+  },
+  {
+    "revision": "a0f82d0730505afc4454e3ae5618cc7f",
+    "url": "/myreactportfolio/static/media/3.a0f82d07.jpg"
+  },
+  {
+    "revision": "8b9e8474f669489fe6ff20f20fd1c7c0",
+    "url": "/myreactportfolio/static/media/9.8b9e8474.jpg"
+  },
+  {
+    "revision": "8e44616eb3458db46638a765786a0831",
+    "url": "/myreactportfolio/static/media/Resume.8e44616e.pdf"
+  },
+  {
+    "revision": "5a1ea214834e07e4082c1c83f92ec6c3",
+    "url": "/myreactportfolio/static/media/a.5a1ea214.jpg"
+  },
+  {
+    "revision": "434bb839008a73df9744d1b66f6e992c",
+    "url": "/myreactportfolio/static/media/c.434bb839.jpg"
+  },
+  {
+    "revision": "6d3e94bebcbb90abd6017edbe8717902",
+    "url": "/myreactportfolio/static/media/d.6d3e94be.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/myreactportfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/myreactportfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/myreactportfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/myreactportfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/myreactportfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
